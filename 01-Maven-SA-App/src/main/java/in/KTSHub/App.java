a1f956d43134b7eef69f142a184855bd89d7cdd6
IrshuKT <irshad2934@gmail.com>
2024-09-24 21:52:47 +0530

    Maven initial commit

---
+++ 01-Maven-SA-App/src/main/java/in/KTSHub/App.java
@@ -0,0 +1,13 @@
+package in.KTSHub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to KTS Hub" );
+    }
+}
